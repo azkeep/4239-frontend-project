9f693a57d700eddb479777b4481d74f812a76c77
Vyacheslav Koldovskyy <koldovsky@koldovsky.com>
2025-06-20 17:17:19 +0300

    Update tasks-01.js

--- js-tasks/tasks-01.js
+++ js-tasks/tasks-01.js
@@ -1,8 +1,8 @@
 // https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
 // by Oleksandr Braiko
 
-function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
-  return (laLigaGoals + copaDelReyGoals + championsLeagueGoals);
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
 }
 
 // https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
@@ -15,15 +15,15 @@ function makeNegative(num) {
 // https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
 // by Oleksandr Braiko
 
-function move (position, roll) {
-  return position + (roll * 2);
+function move(position, roll) {
+  return position + roll * 2;
 }
 
 // https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
 // by Oleksandr Braiko
 
-function greet (name, owner) {
-  return `Hello ${name === owner ? 'boss' : 'guest'}`
+function greet(name, owner) {
+  return `Hello ${name === owner ? "boss" : "guest"}`;
 }
 
 // https://www.codewars.com/kata/keep-hydrated-1/train/javascript
@@ -36,6 +36,6 @@ function litres(time) {
 // https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
 // by Oleksandr Braiko
 
-function lovefunc(flower1, flower2){
-  return ((flower1 % 2) !== (flower2 % 2));
+function lovefunc(flower1, flower2) {
+  return flower1 % 2 !== flower2 % 2;
 }
