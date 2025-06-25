22d516e363ef056fb1f9bf7fbc865263214b4d57
Oleksandr-Braiko <azkeep@gmail.com>
2025-06-25 21:35:36 +0300

    Link index.js to index.html; Add index.popular-artists.js; Link podcasts.js to podcasts.html; Add podcasts.about.js;

---
+++ js/index.js
@@ -0,0 +1,11 @@
+function init() {
+    import('./index.popular-artists.js');      /* by Oleksandr Braiko */
+}
+
+const totalPartials = document.querySelectorAll('[hx-trigger="load"], [data-hx-trigger="load"]').length;
+let loadedPartialsCount = 0;
+
+document.body.addEventListener('htmx:afterOnLoad', () => {
+    loadedPartialsCount++;
+    if (loadedPartialsCount === totalPartials) init();
+});
