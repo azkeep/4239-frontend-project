58a6ab834375fece2b4e91336ee1f5adbc4bb6bf
Olexii <leshalol66@gmail.com>
2025-07-01 18:24:43 +0300

    Created contacts.js and imported two js files to it

--- js/contacts.js
+++ js/contacts.js
@@ -1,9 +1,6 @@
 function init() {
-    import('./index.popular-artists.js');      /* by Oleksandr Braiko */
-    import('./index.testimonials.js');
-    import('./index.popular-music-carousel.js');
-    import('./index.popular-songs.script.js'); /* by Viktoriia Remeniuk */
-    import('./global-header-toggle.js')
+    import('./contacts.header-hero.js');
+    import('./contacts.register.js');
 }
 
 const totalPartials = document.querySelectorAll('[hx-trigger="load"], [data-hx-trigger="load"]').length;
